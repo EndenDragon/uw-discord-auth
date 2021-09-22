32069a80e792af1bd9f6c8e62ac4d8b78f6fa00c
Jeremy Zhang <jkzhang@uw.edu>
2021-09-21 21:32:44 -0700

    Add initial files

---
+++ config.example.py
@@ -0,0 +1,16 @@
+config = {
+    "app-secret": "blah",
+
+    "client-id": "stuff",
+    "client-secret": "stuff",
+    "bot-token": "stuff",
+
+    "discords": [
+        {
+            "guild_id": "140252024666062848",
+            "log_channel": "140252024666062848",
+            "role_remove": ["890091791880179783"],
+            "role_add": ["890091893688500264"],
+        },
+    ]
+}
