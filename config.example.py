aac8e906fb1f4fb394274c9e49708a4b02ee3a07
Jeremy "EndenDragon" Zhang <jeremyzhang5000@gmail.com>
2021-12-21 21:51:42 -0800

    Add context to config

--- config.example.py
+++ config.example.py
@@ -1,10 +1,13 @@
 config = {
+    # Any text works here, used to secure the session cookie
     "app-secret": "blah",
 
+    # Get these values from Discord Developer Portal website
     "client-id": "stuff",
     "client-secret": "stuff",
     "bot-token": "stuff",
 
+    # Get these values from Discord app Copy ID context menu
     "discords": [
         {
             "guild_id": "140252024666062848",
